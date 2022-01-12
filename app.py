3c72fa8d14c6b9fda2da0edda9588099939185e1
seungjaelee <dhssnfl258@inha.edu>
2022-01-12 13:31:07 +0900

    회원가입 중복확인 수정

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, jsonify, request, redirect, url_for
 import hashlib
 import datetime
 import jwt
@@ -17,9 +17,26 @@ client = MongoClient('localhost', 27017)
 db = client.cp
 
 ## HTML 화면 보여주기
+# @app.route('/')
+# def home():
+#     return render_template('main.html')
+
 @app.route('/')
 def home():
-    return render_template('main.html')
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        user_info = db.users.find_one({"username": payload["id"]})
+        return render_template('main.html', user_info=user_info)
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
+    except jwt.exceptions.DecodeError:
+        return render_template('main.html')
+
+
+
+
+
 ##로그인 화면
 @app.route('/login')
 def login_home():
@@ -76,42 +93,36 @@ def sign_in():
          'id': username_receive,
          'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
         }
-        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256').decode('utf-8')
-
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+ #.decode('utf-8')
         return jsonify({'result': 'success', 'token': token})
     # 찾지 못하면
     else:
-        return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
+        return jsonify({'result': 'fail', 'msg': '아이디또는 비밀번호가 일치하지 않습니다.'})
 
-    #회원가입
-    @app.route('/sign_up/save', methods=['POST'])
-    @app.route('/sign_up/check_dup', methods=['POST'])
-    def check_dup():
-        username_receive = request.form['username_give'] #프론트단에서 받은 값을 findone을 통하여 중복확인 검사.
-        exists = bool(db.users.find_one({"username": username_receive}))
-        return jsonify({'result': 'success', 'exists': exists})
+@app.route('/sign_up/save', methods=['POST'])
+def sign_up():
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
+    password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    doc = {
+        "username": username_receive,                               # 아이디
+        "password": password_hash,                                  # 비밀번호
+        "profile_name": username_receive,                           # 프로필 이름 기본값은 아이디
+    }
+    db.users.insert_one(doc)
+    return jsonify({'result': 'success'})
 
-    @app.route('/sign_up/save', methods=['POST'])
-    def sign_up():
-        username_receive = request.form['username_give']
-        password_receive = request.form['password_give']
-        password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
-        doc = {
-            "username": username_receive,                               # 아이디
-            "password": password_hash,                                  # 비밀번호
-            "profile_name": username_receive,                           # 프로필 이름 기본값은 아이디
-            "nickname" : ""                                             # 닉네임
-            }
-        db.users.insert_one(doc)
-        return jsonify({'result': 'success'})
 
 #아이디중복확인
 @app.route('/sign_up/check_dup', methods=['POST'])
 def check_dup():
-    username_receive = request.form['username_give']
+    username_receive = request.form['username_give'] #클라이언트로부터 유저네임을 받는다.
     exists = bool(db.users.find_one({"username": username_receive}))
     return jsonify({'result': 'success', 'exists': exists})
 
+
+
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5002, debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
 
